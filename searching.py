cd4b43541c98ea4df109c08dcca2de0c1c8c8e5f
284547 <284547@vutbr.cz>
2026-04-07 13:26:50 +0200

    1. odevzdani

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -11,11 +12,25 @@ def read_data(file_name, field):
     :param field: (str), field of a dict to return
     :return: (list, string),
     """
+    if field != "unordered_numbers":
+        return None
+
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, 'r', encoding='utf-8') as json_file:
+        seq = json.load(json_file)
+    return seq[field]
+
+def linear_search(sekvence, number):
+    position = []
+    count = 0
+
+
 
 
 def main():
-    pass
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print(sequential_data)
+
 
 
 if __name__ == '__main__':
